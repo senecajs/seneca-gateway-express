52a379e1da8f3a554d48cd80f7e9e2ce7ace3482
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-09-14 11:37:36 +0300

    Clarify the intention of the changes via comments

--- dist/gateway-express.js
+++ dist/gateway-express.js
@@ -22,11 +22,25 @@ function gateway_express(options) {
                 return next();
             }
             if (out === null || out === void 0 ? void 0 : out.error$) {
+                // NOTE: Here we are passing the object with information about
+                // the error to the Express error handler, which allows users
+                // to handle errors in their Express error handler.
+                //
+                // This is useful, for example, when you want to return an HTTP
+                // 404 for the 'act_not_found' error; - or any other user-defined
+                // error thrown inside a Seneca instance.
+                //
                 return next(out);
             }
             return res.send(out);
         }
         catch (err) {
+            /* NOTE: Since the `gateway` function should not normally throw, and
+             * all errors coming from the underlying Seneca instance are handled
+             * by it, - we assume that if something does throw, it's nothing that
+             * can be adequately handled. Hence, we do not pass this error to the
+             * Express handler.
+             */
             throw err;
         }
     }
